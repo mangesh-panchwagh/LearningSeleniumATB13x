261e49b0251eecdc0a8f9d023b2fc5d5e53bdf08
Mangesh Panchwagh <panchwaghmangesh177@gmail.com>
2025-09-21 11:06:39 +0530

    dry run

---
+++ src/test/java/ex01_Selenium_Basics/TestSelenium01.java
@@ -0,0 +1,13 @@
+package ex01_Selenium_Basics;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class TestSelenium01 {
+
+	@Test
+	public void test_code() {
+		
+		Assert.assertEquals("mangesh", "panchwagh");
+	}
+}
