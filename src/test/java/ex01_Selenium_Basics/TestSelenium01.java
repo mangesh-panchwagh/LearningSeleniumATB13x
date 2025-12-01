533a78eee56dcd08931f1fad332f0e02076684ee
Mangesh Panchwagh <panchwaghmangesh177@gmail.com>
2025-12-01 11:21:12 +0530

    updated .gitignore

--- src/test/java/ex01_Selenium_Basics/TestSelenium01.java
+++ src/test/java/ex01_Selenium_Basics/TestSelenium01.java
@@ -1,6 +1,7 @@
 package ex01_Selenium_Basics;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.Assert;
 import org.testng.annotations.Test;
@@ -17,5 +18,8 @@ public class TestSelenium01 {
     public void test_OpenTheTestingAcademy(){
         FirefoxDriver driver = new FirefoxDriver();
         driver.get("https://thetestingacademy.com");
+        
+        ChromeDriver driver2 = new ChromeDriver();
+        driver2.get("https://thetestingacademy.com");
     }
 }
