0c847b6614357332123e235a1a1ceeffce42bcf5
Mangesh Panchwagh <panchwaghmangesh177@gmail.com>
2025-12-01 10:42:12 +0530

    added gitignore file

--- src/test/java/ex01_Selenium_Basics/TestSelenium01.java
+++ src/test/java/ex01_Selenium_Basics/TestSelenium01.java
@@ -1,5 +1,7 @@
 package ex01_Selenium_Basics;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
@@ -8,6 +10,12 @@ public class TestSelenium01 {
 	@Test
 	public void test_code() {
 		
-		Assert.assertEquals("mangesh", "panchwagh");
+		Assert.assertEquals("Mangesh", "Mangesh");
 	}
+	
+	@Test
+    public void test_OpenTheTestingAcademy(){
+        FirefoxDriver driver = new FirefoxDriver();
+        driver.get("https://thetestingacademy.com");
+    }
 }
